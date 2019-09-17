ad84da4c4ab04b2c5d54817cac97b011350d00bf
bafiam <stevebafiam@gmail.com>
2019-09-17 16:56:40 +0300

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -56,6 +56,19 @@ class ProductionConfig(Config):
     """configuration for the production environment"""
     # DB_NAME = os.getenv('DB_NAME')
     DEBUG = True
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+
+    # Define the database - we are working with
+    # SQLite for this example
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'EESBS.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATABASE_CONNECT_OPTIONS = {}
+
+    # Application threads. A common general assumption is
+    # using 2 per available processor cores - to handle
+    # incoming requests using one and performing background
+    # operations using the other.
+    THREADS_PER_PAGE = 2
 
 
 app_config = {
